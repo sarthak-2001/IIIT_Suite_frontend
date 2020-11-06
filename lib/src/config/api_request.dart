22d4482246bd1ff716c3d60fc6188d0402b2840a
sarthak-2001 <sarthakbrahma2001@gmail.com>
2020-11-06 23:59:43 +0530

    api changes

--- lib/src/config/api_request.dart
+++ lib/src/config/api_request.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
-String attendance = "https://sarthak-mums-iiit.herokuapp.com/attendance";
-String bookSearch = "https://sarthak-mums-iiit.herokuapp.com/booksrch";
-String user = "https://sarthak-mums-iiit.herokuapp.com/login";
-String faculty = "https://sarthak-mums-iiit.herokuapp.com/facList";
-String grade = "https://sarthak-mums-iiit.herokuapp.com/grades";
-String intranet = "https://sarthak-mums-iiit.herokuapp.com/intra";
-String notice = "https://sarthak-mums-iiit.herokuapp.com/notices";
-String semesters = "https://sarthak-mums-iiit.herokuapp.com/sem";
-String student = "https://sarthak-mums-iiit.herokuapp.com/students";
-String name = "https://sarthak-mums-iiit.herokuapp.com/name";
+String attendance = "http://placement.iiit-bh.ac.in/api/hibi/attendance";
+String bookSearch = "http://placement.iiit-bh.ac.in/api/hibi/booksrch";
+String user = "http://placement.iiit-bh.ac.in/api/hibi/login";
+String faculty = "http://placement.iiit-bh.ac.in/api/hibi/facList";
+String grade = "http://placement.iiit-bh.ac.in/api/hibi/grades";
+String intranet = "http://placement.iiit-bh.ac.in/api/hibi/intra";
+String notice = "http://placement.iiit-bh.ac.in/api/hibi/notices";
+String semesters = "http://placement.iiit-bh.ac.in/api/hibi/sem";
+String student = "http://placement.iiit-bh.ac.in/api/hibi/students";
+String name = "http://placement.iiit-bh.ac.in/api/hibi/name";
